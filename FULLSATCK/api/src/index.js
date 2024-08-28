176b2cae7b9907f9cc80acd34a4e49553db3f18e
cbougma <cecile.bougma@atos.net>
2024-08-29 00:42:41 +0200

    mise en place src api

--- FULLSATCK/api/src/index.js
+++ FULLSATCK/api/src/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const app = express();
 let count;
-const MongoUrl= process.env.MODE_ENV==='production' ? ``:
-`mongodb://db`
+const uri = process.env.NODE_ENV === 'production' ?
+`mongodb://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PWD }@db` :
+`mongodb://db`;
 
-console.log(process.env);
-//'mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@db'
-
-const client = new MongoClient(MongoUrl);
+const client = new MongoClient(uri);
 async function run() {
   try {
     await client.connect();
@@ -21,18 +18,16 @@ async function run() {
 }
 run().catch(console.dir);
 
+const app = express();
+
 app.get('/api/count', (req, res) => {
-  count
-    .findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true, upsert: true })
-    .then((doc) => {
-      res.status(200).json(doc ? doc.count : 0);
-    });
+  count.findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true, upsert: true }).then((doc) => {
+    res.status(200).json(doc ? doc.count : 0);
+  })
+})
 
-
-});
-
-app.all('*', (req, res) => {  
-    res.status(404).end();
-  });
+app.all('*', (req, res) => {
+  res.status(404).end();
+})
 
 app.listen(80);
